a694aa4203d3826d435a06f6bbb155d7eb4b9f2e
fluxsec7-oss <fluxsec7@gmail.com>
2025-11-25 21:02:02 +0100

    Simplificar configuración para despliegue en Cloudflare Workers

--- _worker.js
+++
@@ -1,9 +0,0 @@
-export default {
-  async fetch(request, env) {
-    try {
-      return await env.ASSETS.fetch(request);
-    } catch (e) {
-      return new Response("Not found", { status: 404 });
-    }
-  }
-};
